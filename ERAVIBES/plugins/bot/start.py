315403236c4e68ec6ecd1aef5c1cc126d6eba0f6
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-16 10:51:25 +0530

    Update start.py

--- ERAVIBES/plugins/bot/start.py
+++ ERAVIBES/plugins/bot/start.py
@@ -24,16 +24,8 @@ from config import BANNED_USERS
 from strings import get_string
 
 IMAGE = [
-"https://files.catbox.moe/ciqfdq.jpg",
-"https://files.catbox.moe/jrfn5c.jpg",
-"https://files.catbox.moe/tb20q7.jpg",
-"https://files.catbox.moe/htuibk.jpg",
-"https://files.catbox.moe/rpghqf.jpg",
-"https://files.catbox.moe/70p4kt.jpg",
-"https://files.catbox.moe/cbr208.jpg",
-"https://files.catbox.moe/3fhcv2.jpg",
-"https://files.catbox.moe/3m0pth.jpg",
-"https://files.catbox.moe/pbekqf.jpg",
+"https://envs.sh/kJw.jpg",
+"https://envs.sh/kSk.jpg",
 ]
 
 # Updated list with emojis
