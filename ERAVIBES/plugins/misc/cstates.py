86f8e6147917d1127de8dc0da50abf07ac944083
Theradhe <radhehu1337@gmail.com>
2024-12-18 14:04:14 +0530

    Update cstates.py

--- ERAVIBES/plugins/misc/cstates.py
+++ ERAVIBES/plugins/misc/cstates.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/DuruXbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/RADHE_MUSUC_ROBOT?startgroup=true"),
     ],
 ]
 
