409a5b2a6ca7bac4d4aa30bc50dbb19645d1539f
Theradhe <radhehu1337@gmail.com>
2024-12-18 13:56:11 +0530

    Update approve.py

--- ERAVIBES/plugins/bot/approve.py
+++ ERAVIBES/plugins/bot/approve.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/DuruXbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/RADHE_MUSIC_ROBOT?startgroup=true"),
     ],
 ]
 
